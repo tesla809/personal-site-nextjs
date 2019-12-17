69be420970a36eccaa721432d7b2aef4d0a2e124
anthony.albertorio <anthony.albertorio@gmail.com>
2019-12-17 16:32:22 -0500

    alerted nav.js with link to github

--- components/nav.js
+++ components/nav.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Link from 'next/link'
 
 const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
+  { href: '/', label: 'Code' },
+  { href: 'https://github.com/tesla809', label: 'GitHub' },
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`
   return link
