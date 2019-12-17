16f6a07171c23d7cd09e3f3490699fd036cd578b
anthony.albertorio <anthony.albertorio@gmail.com>
2019-12-17 16:32:51 -0500

    changed around index.js from default. Still alot of work to do.

--- pages/index.js
+++ pages/index.js
@@ -12,26 +12,24 @@ const Home = () => (
     <Nav />
 
     <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
+      <h1 className="title">Dance!</h1>
       <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
+        Play around with the <code>code</code> to explore.
       </p>
 
       <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
+        <a href="https://medium.com/@tesla809" className="card">
+          <h3>Blog</h3>
+          <p>Blah Blah Stuff</p>
         </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
+        <a href="/" className="card">
+          <h3>Code</h3>
+          <p>Explore Computational Magic</p>
         </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
+        <a href="/"
+          className="card">
+          <h3>Random</h3>
+          <p>Are you feeling lucky?</p>
         </a>
       </div>
     </div>
