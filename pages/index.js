6f1d5280fa2835018155a51db0744609c7a18c7d
anthony.albertorio <anthony.albertorio@gmail.com>
2019-12-21 15:27:13 -0500

    moved p5.js code to new component called graphics.js

--- pages/index.js
+++ pages/index.js
@@ -2,28 +2,9 @@ import React, { Component } from 'react';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import Nav from '../components/nav';
-import sketch from '../sketches/sketch';
-
-// needed to deal with server side rending 
-const P5Wrapper = dynamic(import('react-p5-wrapper'), {
-  ssr: false,  // guess we turn it off to avoid reference error for window, 
-               // since node doesn't have that context
-  loading: () => (
-    <div className="sketch-holder">Loading...</div>
-  ),
-});
+import Graphics from '../components/graphics';
 
 class Home extends Component {
-  constructor(){
-    super();
-    this.state = {color:[Math.random()*255, Math.random()*255, Math.random()*255]};
-    this.randomColor = this.randomColor.bind(this);
-  }
-
-  randomColor(){
-    this.setState({color:[Math.random()*255, Math.random()*255, Math.random()*255]});
-  }
-
   render() {
     return (
       <div>
@@ -33,11 +14,8 @@ class Home extends Component {
         </Head>
     
         <Nav />
-        
-        <div>
-          <button onClick={this.randomColor}>Random Color</button>
-          <P5Wrapper sketch={sketch} color={this.state.color}></P5Wrapper>
-        </div>
+
+        <Graphics/>
     
         <div className="hero">
           <h1 className="title">Dance!</h1>
