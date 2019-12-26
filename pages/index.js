8a041035ef29fa6908226e52694677fe85d0723c
anthony.albertorio <anthony.albertorio@gmail.com>
2019-12-25 20:46:59 -0500

    added css and pass it to components

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import Home from '../components/Home';
 import Graphics from '../components/Graphics';
 
+
 class Index extends Component {
   render() {
+    const Wrapper = styled.div`
+      position: relative;
+    `;
+
+    const homeStyle = {
+      backgroundColor: "orange",
+      position: "absolute",
+      margin: "auto",
+      width: "100%",
+      zIndex: 9,
+      opacity: 0.6,
+    };
+
+    const graphicsStyle = {
+      backgroundColor: "beige",
+      position: "absolute",
+      margin: "auto",
+      left: "25%",
+      width: "50%",
+    };
+
     return (
       <div>
-        <Graphics></Graphics>
-        <Home></Home>
+        <Wrapper>
+          <Graphics style={graphicsStyle} />
+          <Home style={homeStyle} />
+        </Wrapper>
       </div>
     );
   }
